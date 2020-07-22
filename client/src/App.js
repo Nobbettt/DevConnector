90bd6ebd71685663722e2130687d27d34d4c4907
Norbert <norbert.laszlo98@gmail.com>
2020-07-22 13:12:40 +0200

    ep 53

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,9 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
+import ProfileForm from './components/profile-form/ProfileForm';
+import AddExperience from './components/profile-form/AddExperience';
+import AddEducation from './components/profile-form/AddEducation';
 import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux';
@@ -33,6 +36,26 @@ const App = () => {
 							<Route exact path='/register' component={Register} />
 							<Route exact path='/login' component={Login} />
 							<PrivateRoute exact path='/dashboard' component={Dashboard} />
+							<PrivateRoute
+								exact
+								path='/create-profile'
+								component={ProfileForm}
+							/>
+							<PrivateRoute
+								exact
+								path='/edit-profile'
+								component={ProfileForm}
+							/>
+							<PrivateRoute
+								exact
+								path='/add-experience'
+								component={AddExperience}
+							/>
+							<PrivateRoute
+								exact
+								path='/add-education'
+								component={AddEducation}
+							/>
 						</Switch>
 					</section>
 				</Fragment>
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,10 +3,14 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
-import { getCurrentProfile } from '../../actions/profile';
+import DashboardActions from './DashboardActions';
+import Experience from './Experience';
+import Education from './Education';
+import { getCurrentProfile, deleteAccount } from '../../actions/profile';
 
 const Dashboard = ({
 	getCurrentProfile,
+	deleteAccount,
 	auth: { user },
 	profile: { profile, loading },
 }) => {
@@ -24,13 +28,23 @@ const Dashboard = ({
 			</p>{' '}
 			{profile !== null ? (
 				<Fragment>
+					<DashboardActions />
+					<Experience experience={profile.experience} />
+					<Education education={profile.education} />
+
+					<div className='my-2'>
+						<button className='btn btn-danger' onClick={() => deleteAccount()}>
+							<i className='fas fa-user-minus'></i> Deltet my account
+						</button>
+					</div>
+				</Fragment>
+			) : (
+				<Fragment>
 					<p>You have not yet setup a profile, plaese add some info</p>
 					<Link to='/create-profile' className='btn btn-primary my-1'>
 						Create Profile
 					</Link>
 				</Fragment>
-			) : (
-				<Fragment>has</Fragment>
 			)}
 		</Fragment>
 	);
@@ -38,6 +52,7 @@ const Dashboard = ({
 
 Dashboard.propTypes = {
 	getCurrentProfile: PropTypes.func.isRequired,
+	deleteAccount: PropTypes.func.isRequired,
 	auth: PropTypes.object.isRequired,
 	profile: PropTypes.object.isRequired,
 };
@@ -47,4 +62,6 @@ const mapStateToProps = (state) => ({
 	profile: state.profile,
 });
 
-export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
+export default connect(mapStateToProps, { getCurrentProfile, deleteAccount })(
+	Dashboard
+);
